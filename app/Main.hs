9e894bf215466a9ce692b4097fe18b948217d776
John Doty <jpd@noqsi.com>
2018-03-04 08:26:16 -0500

    Start CodeGen development

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,14 @@
 module Main where
 import Levy.AST
 import Levy.Parse
+import Levy.CodeGen
+import System.IO (hPutStrLn, stderr)
+import System.Exit (exitFailure)
 
 main :: IO ()
 main = 
   do contents <- getContents
-     let ast = parseLevy contents
-     putStrLn (show ast)
+     case (parseLevy contents) of
+          (Left fail) -> hPutStrLn stderr (show fail) >> exitFailure 
+          (Right ast) -> do putStrLn (show ast) 
+                            putStrLn (codeGen ast)
