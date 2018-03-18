514d2135b66fa04e80463e851f21e94df062ab71
John Doty <jpd@noqsi.com>
2018-03-18 11:39:51 -0400

    Start documentation

--- src/Levy/CodeGen.hs
+++ src/Levy/CodeGen.hs
@@ -2,8 +2,9 @@ module Levy.CodeGen where
 
 import Levy.AST
 import Control.Monad.State.Lazy (State, evalState, get, put)
+import qualified Data.Map as Map
 
-data Env = Env { symbolTable :: Map String Int
+data Env = Env { symbolTable :: Map.Map String Int
                  , nextCounterCell :: Int }
 fresh :: State Env Int
 fresh = do
